e67e081c6aafd99d07b78c7b94f72e177da08c0e
flatnumberh34 <saadfarhan347@gmail.com>
2023-08-16 12:45:56 +0500

    feat: improve styling of the text container div

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import P from '@/components/ui/p'
 
 export default function Home() {
   return (
-    <div className="h-screen bg-main flex items-center justify-center">
+    <div className="h-screen bg-main gap-2 p-5 flex lg:flex-row flex-col items-center justify-center">
       <H1>Saad Farhan</H1>
 			<P>This website is under construction!</P>
     </div>
