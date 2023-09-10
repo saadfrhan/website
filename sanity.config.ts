f1ef79cb71330cf51b8c7a0e4fc7c5fde66e77b0
flatnumberh34 <saadfarhan347@gmail.com>
2023-09-11 02:35:46 +0500

    Update sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,20 +1,15 @@
-import { defineConfig } from "sanity";
-import { deskTool } from "sanity/desk";
-import { schemaTypes } from "./sanity/schemas";
-import {
-    visionTool
-} from '@sanity/vision';
-import { API_VERSION, DATASET, PROJECT_ID } from "./sanity/env";
+import { defineConfig } from 'sanity';
+import { deskTool } from 'sanity/desk';
+import { schemaTypes } from './sanity-cms/schemas';
+import { visionTool } from '@sanity/vision';
+import { API_VERSION, DATASET, PROJECT_ID } from './sanity-cms/env';
 
 export default defineConfig({
-  name: "portfolio",
-  title: "Portfolio",
+  name: 'portfolio',
+  title: 'Portfolio',
   projectId: PROJECT_ID,
   dataset: DATASET,
-  basePath: "/studio",
-  plugins: [
-    deskTool(),
-    visionTool({ defaultApiVersion: API_VERSION })
-  ],
+  basePath: '/studio',
+  plugins: [deskTool(), visionTool({ defaultApiVersion: API_VERSION })],
   schema: { types: schemaTypes },
 });
