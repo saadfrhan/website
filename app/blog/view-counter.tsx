22e3c65a232f641c6ec23eaf3c15599aa0cbd74b
flatnumberh34 <saadfarhan347@gmail.com>
2023-09-11 02:02:02 +0500

    update

---
+++ app/blog/view-counter.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { useEffect } from 'react';
+import { increment } from '@/server-actions/increment-view';
+
+export default function ViewCounter({
+  slug,
+  allViews,
+  trackView,
+}: {
+  slug: string;
+  allViews: {
+    slug: string;
+    count: number;
+  }[];
+  trackView?: boolean;
+}) {
+  const viewsForSlug = allViews && allViews.find((view) => view.slug === slug);
+  const number = new Number(viewsForSlug?.count || 0);
+
+  useEffect(() => {
+    if (trackView) {
+      increment(slug);
+    }
+  }, []);
+
+  return (
+    <p className="text-muted-foreground">
+      {`${number.toLocaleString()} views`}
+    </p>
+  );
+}
---
+++ components/tweet.tsx
@@ -0,0 +1,42 @@
+import { getTweet } from 'react-tweet/api';
+import { EmbeddedTweet, TweetNotFound, type TweetProps } from 'react-tweet';
+import './tweet.css';
+
+const TweetContent = async ({ id, components, onError }: TweetProps) => {
+  let error;
+  const tweet = id
+    ? await getTweet(id).catch((err) => {
+        if (onError) {
+          error = onError(err);
+        } else {
+          console.error(err);
+          error = err;
+        }
+      })
+    : undefined;
+
+  if (!tweet) {
+    const NotFound = components?.TweetNotFound || TweetNotFound;
+    return <NotFound error={error} />;
+  }
+
+  return <EmbeddedTweet tweet={tweet} components={components} />;
+};
+
+export const ReactTweet = (props: TweetProps) => (
+  // I don't want a loading state though... I want to prerender
+  // but I do want an ErrorBoundary
+  // <Suspense fallback={<TweetSkeleton />}>
+  <TweetContent {...props} />
+  // </Suspense>
+);
+
+export async function TweetComponent({ id }: { id: string }) {
+  return (
+    <div className="tweet my-6">
+      <div className={`flex justify-center`}>
+        <ReactTweet id={id} />
+      </div>
+    </div>
+  );
+}
