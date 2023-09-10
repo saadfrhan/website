8838ed6e93b56fe352388c07d64a6131b6560586
flatnumberh34 <saadfarhan347@gmail.com>
2023-09-11 04:47:26 +0500

    Update page.tsx

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -108,7 +108,7 @@ export default async function Blog({
         <Balancer>{post.title}</Balancer>
       </h1>
       <div className="flex justify-between items-center mt-2 mb-8 text-sm">
-        <p className="text-sm text-neutral-600 dark:text-neutral-400">
+        <p className="text-sm text-muted-foreground">
           {formatDate(post.publishedAt)}
         </p>
         <Suspense>
