bc3ae1e324a296d5577d0069d9c3368e92169327
saadfrhan <saadfarhan347@gmail.com>
2023-12-20 01:21:20 +0500

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,83 +1,71 @@
 import './globals.css';
-import { Inter } from 'next/font/google';
-import Navbar from '@/components/layout/navbar';
 import { Analytics } from '@vercel/analytics/react';
 import { Metadata } from 'next';
 import { ThemeProvider } from '@/components/theme-provider';
-import { formatDistance } from 'date-fns';
-import { getLatestCommitDate } from '@/lib/utils';
-
-const inter = Inter({ subsets: ['latin'] });
+import { GeistSans } from 'geist/font/sans'
+import { ThemeToggler } from '@/components/theme-toggler';
 
 const description =
-  'Application developer with coding skills & passion for staying current in the industry.';
+	'Application developer with coding skills & passion for staying current in the industry.';
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://saadfarhan.vercel.app'),
-  title: {
-    default: 'Saad Farhan',
-    template: '%s | Saad Farhan',
-  },
-  description,
-  openGraph: {
-    title: 'Saad Farhan',
-    description:
-      'Application developer with coding skills & passion for staying current in the industry.',
-    url: 'https://saadfarhan.vercel.app',
-    siteName: 'Saad Farhan',
-    locale: 'en_US',
-    type: 'website',
-  },
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
-    },
-  },
-  twitter: {
-    title: 'Saad Farhan',
-    card: 'summary_large_image',
-  },
-  verification: {
-    google: 'zHB4gjR-LZ7okfLYgT9olSwGCyo9geeG3Ky4RUpnr1k',
-    yandex: '30dab239bbb58d8f',
-  },
+	metadataBase: new URL('https://saadfarhan.vercel.app'),
+	title: {
+		default: 'Saad Farhan',
+		template: '%s | Saad Farhan',
+	},
+	description,
+	openGraph: {
+		title: 'Saad Farhan',
+		description:
+			'Application developer with coding skills & passion for staying current in the industry.',
+		url: 'https://saadfarhan.vercel.app',
+		siteName: 'Saad Farhan',
+		locale: 'en_US',
+		type: 'website',
+	},
+	robots: {
+		index: true,
+		follow: true,
+		googleBot: {
+			index: true,
+			follow: true,
+			'max-video-preview': -1,
+			'max-image-preview': 'large',
+			'max-snippet': -1,
+		},
+	},
+	twitter: {
+		title: 'Saad Farhan',
+		card: 'summary_large_image',
+	},
+	verification: {
+		google: 'zHB4gjR-LZ7okfLYgT9olSwGCyo9geeG3Ky4RUpnr1k',
+		yandex: '30dab239bbb58d8f',
+	},
 };
 export default async function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
-      >
-        <body
-          className={`${inter.className} antialiased max-w-2xl flex flex-col md:flex-row mx-auto`}
-        >
-          <main className="flex-auto min-w-0 flex flex-col space-y-3 pb-8 px-4 md:px-0">
-            <Navbar />
-            {children}
-          </main>
-          <div className="fixed max-sm:bottom-2 max-sm:right-2 bottom-5 right-5 text-xs font-medium text-neutral-400 dark:text-neutral-500">
-            Last updated {formatDistance(new Date(await getLatestCommitDate()), new Date(),
-              { addSuffix: true }
-            )
-
-            }
-          </div>
-          <Analytics />
-        </body>
-      </ThemeProvider>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<ThemeProvider
+				attribute="class"
+				defaultTheme="system"
+				enableSystem
+				disableTransitionOnChange
+			>
+				<body
+					className={`${GeistSans.className} antialiased max-w-2xl flex flex-col md:flex-row mx-auto`}
+				>
+					<Analytics />
+					<div className='fixed bottom-0 right-5'>
+						<ThemeToggler />
+					</div>
+				</body>
+			</ThemeProvider>
+		</html>
+	);
 }
--- lib/utils.tsx
+++ lib/utils.tsx
@@ -2,38 +2,32 @@ import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
+	Tooltip,
+	TooltipContent,
+	TooltipTrigger,
 } from '@/components/ui/tooltip';
 import { P } from '@/components/ui/p';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+	return twMerge(clsx(inputs));
 }
 
 export function TooltipWrapper({
-  label,
-  children,
+	label,
+	children,
 }: {
-  label: string;
-  children: React.ReactNode;
+	label: string;
+	children: React.ReactNode;
 }) {
-  return (
-    <Tooltip>
-      <TooltipTrigger asChild>{children}</TooltipTrigger>
-      <TooltipContent>
-        <P>{label}</P>
-      </TooltipContent>
-    </Tooltip>
-  );
+	return (
+		<Tooltip>
+			<TooltipTrigger asChild>{children}</TooltipTrigger>
+			<TooltipContent>
+				<P>{label}</P>
+			</TooltipContent>
+		</Tooltip>
+	);
 }
 
-// get a latest commit date from github repo saadfrhan/website
 
-export async function getLatestCommitDate() {
-  const res = await fetch('https://api.github.com/repos/saadfrhan/website/commits');
-  const json = await res.json();
-  const latestCommitDate = json[0].commit.author.date;
-  return latestCommitDate;
 }
