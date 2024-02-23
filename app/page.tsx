02d2235ee4159c9fff429754c9e0433be745dad9
saadfrhan <saadfarhan347@gmail.com>
2024-02-23 08:34:25 +0500

    init

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+	return (
+		<div className="flex min-h-screen justify-center items-center">
+			<h1 className="max-[252px]:px-4">Hi, this site is under construction.</h1>
+		</div>
+	);
+}
