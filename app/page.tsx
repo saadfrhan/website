c629776ba79a51008ab773cc43493687dd14aae3
Saad Farhan <saadfarhan347@gmail.com>
2024-02-23 10:43:07 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
 	return (
-		<div className="flex min-h-screen justify-center items-center">
-			<h1 className="max-[252px]:px-4">Hi, this site is under construction.</h1>
+		<div className="flex h-screen justify-center items-center">
+			<h1>Hi, this site is under construction.</h1>
 		</div>
 	);
 }
