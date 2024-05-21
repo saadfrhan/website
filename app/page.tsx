2aba964231733352b72a582f7a95b0624dd088b8
saadfrhan <saadfarhan347@gmail.com>
2024-05-21 18:21:59 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,26 +3,16 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="h-dvh flex flex-col items-center justify-center">
+    <div className="h-dvh flex flex-col items-center justify-center space-y-2">
       <h1 className="text-heading font-extrabold text-3xl">Coming Soon</h1>
-      <div className="text-foreground text-sm mt-2 space-x-2">
-        <Link
-          href="https://saadfarhan-old.vercel.app"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-link p-0 underline-offset-4 hover:underline"
-        >
-          Old site
-        </Link>
-        <Link
-          href="https://saadfarhan-bio.vercel.app"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-link p-0 underline-offset-4 hover:underline"
-        >
-          Bio site
-        </Link>
-      </div>
+      <Link
+        href="https://saadfarhan-bio.vercel.app"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-link p-0 text-sm underline-offset-4 hover:underline"
+      >
+        Bio site
+      </Link>
     </div>
   );
 }
